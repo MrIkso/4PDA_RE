7b2ad681340f21ce73171ddd95fa7dd10f11ce30
MrIkso <mrikso821@gmail.com>
2022-03-26 08:09:57 +0200

    update code

---
+++ app/src/main/java/ru/fourpda/client/Stylable.java
@@ -0,0 +1,6 @@
+package ru.fourpda.client;
+
+public final class Stylable {
+    public static final int[] boxStyle = {R.attr.box_align, R.attr.sub_text};
+    public static final int[] Radius = {R.attr.radius};
+}
